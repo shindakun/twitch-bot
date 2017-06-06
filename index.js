d1ab5de8c9632782710be1d6185df657493e3961
System <none>
2017-06-06 21:20:55 +0000

    🐷🦀 Updated with Glitch

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const tmi = require("tmi.js");
+const app = express();
+
+// Glitch expects a web server basically so we're starting express to take care of that.
+app.get("/", function (request, response) {
+  response.send('Hello.');
+});
+
+let listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
+// Setting options for our bot, disable debug output once your up and running.
+let options = {
+  options: {
+    debug: true
+  },
+  connection: {
+    cluster: "aws",
+    reconnect: true
+  },
+  identity: {
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD
+  },
+    channels: ["shinbot"]
+};
+
+// Set up our new TMI client and connect to the server.
+let client =  new tmi.client(options);
+client.connect();
+
+// We have debug enabled now but if not we want some sort of confirmation
+// we've connected to the server.
+client.on('connected', (address, port) => {
+  console.log(`Connected to ${address}:${port}`);
+})
+
+// This simple bot will simply monitor chat logging for instances of '!twitter' or '!github'.
+// 
+client.on('chat', (channel, user, message, self) => {
+  switch(message) {
+    case '!twitter': 
+      client.action('shinbot', `${user['display-name']} you can find it at twitter.com/shindakun`);
+      break;
+    case '!github': 
+      client.action('shinbot', `${user['display-name']} you can find it at github.com/shindakun`);
+      break;
+    default:
+      break;
+  }
+})
